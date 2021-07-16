e2e7b695223a370a5937eb5693ae8b2384e6a6d8
Eolien55 <elielevaillant2007@gmail.com>
2021-07-16 19:30:29 +0200

    rewrote yt-dl

--- youtube-download.py
+++ youtube-download.py
@@ -1,81 +1,52 @@
-import subprocess
-import click
+from subprocess import PIPE, Popen
 from typing import Union
-from pytube import YouTube, Playlist, Stream
+import pytube
+from pytube import YouTube, Playlist, Stream, StreamQuery
 import os
 from html import unescape
 from werkzeug.utils import secure_filename as sane_file
 import ffmpeg
 from multiprocessing import Process as Thread
-from shutil import move
+import click
+import shutil
 
 user = os.getlogin()
 
 
-def get_highest_resolution(video: YouTube, best):
-    if (supposed_highest := video.streams.get_highest_resolution()) != (
-        true_highest := video.streams.order_by("resolution").last()
-    ) and best:
-        return (
-            true_highest,
-            video.streams.order_by("abr").last(),
-        ), true_highest.resolution
-    return supposed_highest, supposed_highest.resolution
+def get_highest_resolution(streams: StreamQuery) -> Union[Stream, tuple[Stream]]:
+    if streams.get_highest_resolution() != (
+        true_best := streams.order_by("resolution").last()
+    ):
+        return (true_best, streams.order_by("abr").last())
+    return true_best
 
 
-def download(video: Union[Stream, tuple], path, name, thread_nb):
+def download(
+    video: Union[Stream, tuple[Stream]], path: str, name: str, thread_nb
+) -> str:
     if type(video) == tuple:
-        formats = (
-            "." + video[0].mime_type.split("/")[1],
-            "." + video[1].mime_type.split("/")[1],
-        )
-        audio_name, video_name = (
-            sane_file(name) + formats[1],
-            sane_file(name) + formats[0],
-        )
-        video[0].download(path, filename=sane_file(name))
-
-        if len(set(formats)) == 1:
-            move(os.path.join(path, video_name), os.path.join(path, "0" + video_name))
-            video_name = "0" + video_name
-
-        video[1].download(path, filename=sane_file(name))
-
-        print(f"[{thread_nb}] Starting to re-encode video file")
-
-        ffmpeg.input(os.path.join(path, video_name)).output(
-            os.path.join(path, "0" + ".".join(video_name.split(".")[:-1]) + ".mp4")
-        ).global_args("-y").global_args("-loglevel", "error").run()
-
-        os.remove(os.path.join(path, video_name))
-        video_name = "0" + ".".join(video_name.split(".")[:-1]) + ".mp4"
-        print(f"[{thread_nb}]Starting to merge audio & video")
-
-        subprocess.run(
+        video_stream = video[0]
+        audio_stream = video[1]
+        video_name = video_stream.download(path, filename="0" + sane_file(name))
+        audio_name = audio_stream.download(path, filename=sane_file(name))
+        Popen(
             [
-                "ffmpeg",
+                "ffmpeg-bar",
                 "-i",
-                os.path.join(path, video_name),
+                video_name,
                 "-i",
-                os.path.join(path, audio_name),
-                "-c:v",
-                "copy",
-                "-c:a",
-                "aac",
-                "-loglevel",
-                "error",
-                "-y",
-                os.path.join(path, "000" + name + ".mp4"),
+                audio_name,
+                os.path.join(path, "00" + sane_file),
             ],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=PIPE,
         )
-        os.remove(os.path.join(path, audio_name))
-        os.remove(os.path.join(path, video_name))
-        return "000" + name + ".mp4"
-    video.download(path, filename=name)
-    return name + ".mp4"
+        os.remove(video_name)
+        os.remove(audio_name)
+        return os.path.join(path, "00" + sane_file)
+
+    return video.download(path, filename=name)
 
 
 def make_format(name: str, name_second: str, path: str) -> None:
@@ -96,11 +67,7 @@ def make_format(name: str, name_second: str, path: str) -> None:
 
 
 def rundownload(
-    link: Union[str, tuple],
-    suffix: str = "",
-    format: str = "mp4",
-    thread_nb: int = 0,
-    best: bool = False,
+    link: Union[str, tuple], suffix: str = "", format: str = "mp4", thread_nb: int = 0
 ) -> None:
     """Actual function for downloading a youtube video
 
@@ -122,7 +89,7 @@ def rundownload(
             assert results, "Aucun résultats"
             result = results[0]
             link = "https://youtube.com/" + result["url_suffix"]
-            rundownload(link, format=format, thread_nb=thread_nb)
+            rundownload(link, format=format)
             return
 
         if link.startswith("https://www.youtube.com/playlist"):
@@ -131,14 +98,14 @@ def rundownload(
             playlist = Playlist(link)
             print(
                 f"[{thread_nb}] Let's download '{playlist.title}{' as ' + format} files !",
+                "",
             )
             for url in playlist.video_urls:
-                rundownload(
-                    url, unescape(playlist.title), format=format, thread_nb=thread_nb
-                )
+                rundownload(url, unescape(playlist.title), format=format)
 
             print(
                 f"[{thread_nb}] Done downloading '{playlist.title}{' as ' + format} files !",
+                "",
             )
 
             return
@@ -160,16 +127,17 @@ def rundownload(
         if format != "mp4":
             name = sane_file(name).replace(".", "")
 
-        download_video, resolution = get_highest_resolution(video, best)
+        download_video = get_highest_resolution(video.streams)
         print(
-            f"[{thread_nb}] Let's download '{video.title}' with a resolution of '{resolution}'{' as a ' + format} !",
+            f"[{thread_nb}] Let's download '{video.title}' with a resolution of '{download_video.resolution}'{' as a ' + format} !",
+            "",
         )
         name = download(
             download_video,
             os.path.join(f"/home/{user}/Desktop/YoutubeVideos", suffix),
             name,
             thread_nb,
-        )
+        ).strip("/")[-1]
 
         # As pytube downloads videos as mp4 files, we don't need further processing
         # if the target format already is mp4
@@ -180,64 +148,55 @@ def rundownload(
                 os.path.join(f"/home/{user}/Desktop/YoutubeVideos", suffix),
             )
         else:
-            move(
+            shutil.move(
                 os.path.join(
                     os.path.join(f"/home/{user}/Desktop/YoutubeVideos", suffix), name
                 ),
                 os.path.join(
                     os.path.join(f"/home/{user}/Desktop/YoutubeVideos", suffix),
-                    video.title + ".mp4",
+                    name_second,
                 ),
             )
         print(
-            f"[{thread_nb}] Done downloading '{video.title}' with a resolution of '{resolution}'{' as a ' + format} !",
+            f"[{thread_nb}] Done downloading '{video.title}' with a resolution of '{download_video.resolution}'{' as a ' + format} !",
+            "",
         )
     except Exception as e:
         # Showing error (probably temporary, i should make error management)
-        print(f"[{thread_nb}] Error : ", e)
+        print(f"[{thread_nb}] Error", e)
 
 
-@click.command(help="Downloads youtube video based on the given url")
+@click.command()
 @click.option(
-    "-u",
     "--url",
-    type=str,
-    metavar="URL",
-    multiple=True,
-    help="Sets the URL to download",
+    "-u",
     required=True,
-)
-@click.option(
-    "--format",
-    "-f",
-    default="mp4",
-    help="Sets which format to save the video to",
-    metavar="FORMAT",
+    help="Which url to download",
+    multiple=True,
+    metavar="URL",
 )
 @click.option(
     "--search/ --nosearch",
     "-s/ -n",
     is_flag=True,
-    help="Search for the url before downloading",
     multiple=True,
+    help="Sets wether the video has to be searched for or not",
 )
 @click.option(
-    "--best", "-b", is_flag=True, help="Sets the video to be absolutely the best it can"
+    "--format",
+    "-f",
+    help="Sets in which format the video has to be saved",
+    metavar="FORMAT",
 )
-def main(url, format, search, best):
-    if len(search) < len(url):
+def main(url, search, format):
+    if len(search) != len(url):
         print(
-            "Uh, there are several options where you didn't set the search flag ; thus, i can't find out which ones need to be searched and which ones do not"
+            "You must have done something wrong, the amount of urls and of search/nosearch flag isn't concordant"
         )
         exit(1)
-    search = search[: len(url)]
     thread_nb = 0
     for the_url, the_search in zip(url, search):
-        the_url = the_url.replace("+", " ")
         if the_search:
             the_url = (the_url,)
-        Thread(target=rundownload, args=(the_url, "", format, thread_nb, best)).start()
+        Thread(target=rundownload, args=(the_url, "", format, thread_nb)).start()
         thread_nb += 1
-
-
-main()
